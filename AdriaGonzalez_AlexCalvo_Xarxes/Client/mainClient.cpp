b7b7ae5f3882baa2dcab15078ae18cf5c05eeb1b
Alex Calvo Menchon <96calvo@gmail.com>
2018-03-17 20:46:35 +0100

    Taulell de joc implementat, amb chat separat, no funcionen a la vegada

--- AdriaGonzalez_AlexCalvo_Xarxes/Client/mainClient.cpp
+++ AdriaGonzalez_AlexCalvo_Xarxes/Client/mainClient.cpp
@@ -5,9 +5,251 @@
 #include <vector>
 #include <thread>
 #include <cstring>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+
 #define MAX_MENSAJES 30
+
+#define MAX 100
+#define SIZE_TABLERO 64
+#define SIZE_FILA_TABLERO 8
+#define LADO_CASILLA 64
+#define RADIO_AVATAR 25.f
+#define OFFSET_AVATAR 5
+
+#define SIZE_TABLERO 64
+#define LADO_CASILLA 64
+#define RADIO_AVATAR 25.f
+#define OFFSET_AVATAR 5
+
 using namespace sf;
 using namespace std;
+
+enum TipoProceso { RATON, GATO, PADRE };
+char tablero[SIZE_TABLERO];
+
+/**
+* Si vale true --> nos permite marcar casilla con el mouse
+* Si vale false --> No podemos interactuar con el tablero y aparece un letrero de "esperando"
+*/
+bool tienesTurno = true;
+
+/**
+* Ahora mismo no tiene efecto, pero luego lo necesitarás para validar los movimientos
+* en función de si eres el gato o el ratón.
+*/
+TipoProceso quienSoy = TipoProceso::RATON;
+
+
+
+/**
+* Cuando el jugador clica en la pantalla, se nos da una coordenada del 0 al 512.
+* Esta función la transforma a una posición entre el 0 y el 7
+*/
+sf::Vector2f TransformaCoordenadaACasilla(int _x, int _y)
+{
+	float xCasilla = _x / LADO_CASILLA;
+	float yCasilla = _y / LADO_CASILLA;
+	sf::Vector2f casilla(xCasilla, yCasilla);
+	return casilla;
+}
+
+/**
+* Si guardamos las posiciones de las piezas con valores del 0 al 7,
+* esta función las transforma a posición de ventana (pixel), que va del 0 al 512
+*/
+sf::Vector2f BoardToWindows(sf::Vector2f _position)
+{
+	return sf::Vector2f(_position.x*LADO_CASILLA + OFFSET_AVATAR, _position.y*LADO_CASILLA + OFFSET_AVATAR);
+}
+
+
+
+
+/**
+* Contiene el código SFML que captura el evento del clic del mouse y el código que pinta por pantalla
+*/
+void DibujaSFML()
+{
+	sf::Vector2f casillaOrigen, casillaDestino;
+	bool casillaMarcada = false;
+
+	sf::RenderWindow window(sf::VideoMode(640, 640), "Entipoly");
+
+	Texture background;
+	if (!background.loadFromFile("../res/Entipoly.png"))
+	{
+		cout << "Error" << endl;
+	}
+	Sprite backgroundSprite;
+	backgroundSprite.setTexture(background);
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		//Este primer WHILE es para controlar los eventos del mouse
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Left && tienesTurno)
+				{
+					int x = event.mouseButton.x;
+					int y = event.mouseButton.y;
+					if (!casillaMarcada)
+					{
+						casillaOrigen = TransformaCoordenadaACasilla(x, y);
+						casillaMarcada = true;
+						//TODO: Comprobar que la casilla marcada coincide con las posición del raton (si le toca al ratón)
+						//o con la posicion de alguna de las piezas del gato (si le toca al gato)
+
+					}
+					else
+					{
+						casillaDestino = TransformaCoordenadaACasilla(x, y);
+						if (casillaOrigen.x == casillaDestino.x && casillaOrigen.y == casillaDestino.y)
+						{
+							casillaMarcada = false;
+							//Si me vuelven a picar sobre la misma casilla, la desmarco
+						}
+						else
+						{
+							if (quienSoy == TipoProceso::RATON)
+							{
+								//TODO: Validar que el destino del ratón es correcto
+
+								//TODO: Si es correcto, modificar la posición del ratón y enviar las posiciones al padre
+
+							}
+							else if (quienSoy == TipoProceso::GATO)
+							{
+								//TODO: Validar que el destino del gato es correcto
+
+								//TODO: Si es correcto, modificar la posición de la pieza correspondiente del gato y enviar las posiciones al padre
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+
+			}
+		}
+
+		window.clear();
+		window.draw(backgroundSprite);
+		//A partir de aquí es para pintar por pantalla
+		//Este FOR es para el tablero
+		/*
+		for (int i = 0; i<10; i++)
+		{
+			for (int j = 0; j<10; j++)
+			{
+				sf::RectangleShape rectBlanco(sf::Vector2f(LADO_CASILLA, LADO_CASILLA));
+				rectBlanco.setFillColor(sf::Color::White);
+				if (i % 2 == 0)
+				{
+					//Empieza por el blanco
+					if (j % 2 == 0)
+					{
+						rectBlanco.setPosition(sf::Vector2f(i*LADO_CASILLA, j*LADO_CASILLA));
+						window.draw(rectBlanco);
+					}
+				}
+				else
+				{
+					//Empieza por el negro
+					if (j % 2 == 1)
+					{
+						rectBlanco.setPosition(sf::Vector2f(i*LADO_CASILLA, j*LADO_CASILLA));
+						window.draw(rectBlanco);
+					}
+				}
+			}
+			
+		}
+		*/
+		Color red(255, 0, 0, 129);
+		Color blue(255, 0, 0, 129);
+		Color green(255, 0, 0, 129);
+		Color yellow(255, 0, 0, 129);
+
+		//TODO: Para pintar el circulito del ratón
+		//Player1
+		sf::CircleShape Player1(RADIO_AVATAR);
+		Player1.setFillColor(red);
+		sf::Vector2f posicionPlayer1(8.f, 8.f);
+		posicionPlayer1 = BoardToWindows(posicionPlayer1);
+		Player1.setPosition(posicionPlayer1);
+		window.draw(Player1);
+		//Player2
+		sf::CircleShape Player2(RADIO_AVATAR);
+		Player2.setFillColor(blue);
+		sf::Vector2f posicionPlayer2(9.f, 8.f);
+		posicionPlayer2 = BoardToWindows(posicionPlayer2);
+		Player2.setPosition(posicionPlayer2);
+		window.draw(Player2);
+		//Player3
+		sf::CircleShape Player3(RADIO_AVATAR);
+		Player3.setFillColor(green);
+		sf::Vector2f posicionPlayer3(8.f, 9.f);
+		posicionPlayer3 = BoardToWindows(posicionPlayer3);
+		Player3.setPosition(posicionPlayer3);
+		window.draw(Player3);
+		//Player4
+		sf::CircleShape Player4(RADIO_AVATAR);
+		Player4.setFillColor(yellow);
+		sf::Vector2f posicionPlayer4(9.f, 9.f);
+		posicionPlayer4 = BoardToWindows(posicionPlayer4);
+		Player4.setPosition(posicionPlayer4);
+		window.draw(Player4);
+
+
+
+		if (!tienesTurno)
+		{
+			//Si no tengo el turno, pinto un letrerito de "Esperando..."
+			sf::Font font;
+			std::string pathFont = "liberation_sans/LiberationSans-Regular.ttf";
+			if (!font.loadFromFile(pathFont))
+			{
+				std::cout << "No se pudo cargar la fuente" << std::endl;
+			}
+
+
+			sf::Text textEsperando("Esperando...", font);
+			textEsperando.setPosition(sf::Vector2f(180, 200));
+			textEsperando.setCharacterSize(30);
+			textEsperando.setStyle(sf::Text::Bold);
+			textEsperando.setFillColor(sf::Color::Green);
+			window.draw(textEsperando);
+		}
+		else
+		{
+			//Si tengo el turno y hay una casilla marcada, la marco con un recuadro amarillo.
+			if (casillaMarcada)
+			{
+				sf::RectangleShape rect(sf::Vector2f(LADO_CASILLA, LADO_CASILLA));
+				rect.setPosition(sf::Vector2f(casillaOrigen.x*LADO_CASILLA, casillaOrigen.y*LADO_CASILLA));
+				rect.setFillColor(sf::Color::Transparent);
+				rect.setOutlineThickness(5);
+				rect.setOutlineColor(sf::Color::Yellow);
+				window.draw(rect);
+			}
+		}
+
+		window.display();
+	}
+
+}
+
 
 
 void receiveData(TcpSocket* socket, vector<string>* aMensajes) {
@@ -86,6 +328,8 @@ int main()
 		}
 	}
 
+	DibujaSFML();
+
 	char data[100];
 
 	std::vector<string> aMensajes;
@@ -119,9 +363,7 @@ int main()
 
 
 	//THREAD RECEIVE seguir per aqui
-
 	thread t1(&receiveData, &socket, &aMensajes);
-
 
 	while (window.isOpen())
 	{
@@ -210,7 +452,5 @@ int main()
 	//socket.disconnect();
 
 	t1.join();
-
 	return 0;
 }
-
